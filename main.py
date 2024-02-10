c6446c8152840bf8c7cba67f690f63fceec89976
Hengzhe Jin <122077361+MakiseChris666@users.noreply.github.com>
2024-02-10 16:09:30 +0800

    archive

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ from modules.frames import login
 import os
 
 if __name__ == '__main__':
-    os.environ['OPENAI_API_KEY'] = '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'
+    os.environ['OPENAI_API_KEY'] = '*'
     login.window.mainloop()
